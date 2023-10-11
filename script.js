4a376e0c4feb1ff2ac888a8a79c3f30c0e1ad018
Syed Quadeer <quadeer.dev@gmail.com>
2023-10-11 19:44:50 +0530

    adding localstorage

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todoUL = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -16,6 +22,7 @@ function addTodo(todo) {
   }
   if (todoText) {
     const todoEl = document.createElement("li");
+
     if (todo && todo.completed) {
       todoEl.classList.add("completed");
     }
@@ -23,12 +30,29 @@ function addTodo(todo) {
 
     todoEl.addEventListener("click", () => {
       todoEl.classList.toggle("completed");
+      updateLS();
     });
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEl.remove();
+      updateLS();
     });
     todoUL.appendChild(todoEl);
     input.value = "";
+    updateLS();
   }
+}
+
+function updateLS() {
+  let todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
